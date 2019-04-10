8510f4245efe68416d6813cc18ef5004da2d4b8c
bcreagh <briancreagh95@gmail.com>
2019-04-10 17:53:23 +0100

    Add ActionResult

---
+++ src/main/java/com/bcreagh/mpspark/mp/domain/ActionResult.java
@@ -0,0 +1,12 @@
+package com.bcreagh.mpspark.mp.domain;
+
+import com.bcreagh.mpspark.mp.utilities.logger.Log;
+
+import java.util.ArrayList;
+
+public class ActionResult {
+    private String input = "";
+    private String output = "";
+    private ArrayList<Log> logs = new ArrayList<>();
+    private int performance = -1;
+}
---
+++ src/main/java/com/bcreagh/mpspark/mp/utilities/logger/Log.java
@@ -0,0 +1,11 @@
+package com.bcreagh.mpspark.mp.utilities.logger;
+
+public class Log {
+    private String level;
+    private String message;
+
+    public Log(String level, String message) {
+        this.level = level;
+        this.message = message;
+    }
+}
